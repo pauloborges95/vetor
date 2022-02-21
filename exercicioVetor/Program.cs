36f36d5b0728d2fbf9a852462738e7f213cfca68
pauloborges95 <pauloborgesmartim@gmail.com>
2022-02-20 23:34:46 -0300

    trabalhando com vetor

---
+++ exercicioVetor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Quarto[] vect = new Quarto[10];
+            Console.WriteLine("Olá informe o número de quartos: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+         
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Eamil: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Quarto ( nome , email );
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("quartos ocupados: ");
+            for(int i = 0; i < 10; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ exercicioVetor/Quarto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioVetor
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Quarto (string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " "+ Email;
+        }
+
+    }
+}
